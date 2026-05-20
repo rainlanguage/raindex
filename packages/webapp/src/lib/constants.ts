1f7b689aac0eef6204f8aea519c813e5d68407d5
Arda Nakisci <anakisci@gmail.com>
2026-05-20 18:39:08 +0200

    chore: update webapp registry hash

--- packages/webapp/src/lib/constants.ts
+++ packages/webapp/src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const REGISTRY_URL =
-  "https://raw.githubusercontent.com/rainlanguage/rain.strategies/805c30972423a9e0cd3d51171344f209011bce22/registry";
+  "https://raw.githubusercontent.com/rainlanguage/rain.strategies/57a40d4f5d83e1faea9bb9570aa80d83260ff7c7/registry";
